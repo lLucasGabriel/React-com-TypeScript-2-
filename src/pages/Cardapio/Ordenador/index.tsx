17a4f5297bc8bf5719d8f87cb49f97d0f7ce17a5
Lucas Gabriel <llucas182011@hotmail.com>
2022-09-20 22:35:47 -0300

    Ordenador

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,11 +1,23 @@
 import styles from "./Ordenador.module.scss";
 import opcoes from "./opcoes.json";
+import { useState } from "react";
+import classNames from "classnames"
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md'
 
 export default function Ordenador() {
+    const [aberto, setAberto] = useState(false);
     return(
-        <button className={styles.ordenador}>
+        <button
+            className={styles.ordenador} 
+            onClick={() => setAberto(!aberto)}
+            onBlur={(() => setAberto(false))}
+        >
             <span>Ordenar por</span>
-            <div className={styles.ordenador__options}>
+            {aberto ? < MdKeyboardArrowUp size={20}/> : < MdKeyboardArrowDown size={20}/>}
+            <div className={classNames({
+                [styles.ordenador__options]: true,
+                [styles["ordenador__options--ativo"]]: aberto, 
+            })}>
             {opcoes.map(opcao => (
                 <div className={styles.ordenador__option} key={opcao.value}>
                     {opcao.nome}
